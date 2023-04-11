b74f17d2c8cadce11713e2c9d96503853fb238ed
Fantomeh <115874284+Fantomeh@users.noreply.github.com>
2023-04-11 11:04:38 +0200

    logement css mobile

--- src/components/HostDetails.js
+++ src/components/HostDetails.js
@@ -1,12 +1,12 @@
-// HostDetails.js
 import React from "react";
+
 
 export const HostDetails = ({ host }) => {
   return (
-    <div>
-      <p className="Host">{host.name}</p>
+    <div className="host-details">
+      <p className="host-name">{host.name}</p>
       {/* Modifier l'attribut alt pour être plus descriptif */}
-      <img className="Host-img" src={host.picture} alt={`Hôte ${host.name}`} />
+      <img className="host-img" src={host.picture} alt={`Hôte ${host.name}`} />
     </div>
   );
 };
