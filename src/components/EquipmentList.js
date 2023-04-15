0bcb18340cb3ebccf3fc5e1c24c834db1b9233db
Fantomeh <115874284+Fantomeh@users.noreply.github.com>
2023-04-15 23:57:46 +0200

    amélioration

--- src/components/EquipmentList.js
+++ src/components/EquipmentList.js
@@ -2,21 +2,18 @@
 import React from "react";
 import ExpandableSection from "./ExpandableSection";
 
-// Déclarer le composant EquipmentList qui prend en paramètre un objet contenant la propriété "equipments"
 export const EquipmentList = ({ equipments }) => {
-  // Retourner un composant ExpandableSection contenant la liste des équipements
   return (
     <div className="List-Destop">
-    <ExpandableSection title="Équipements">
-      {/* Créer un élément div pour contenir la liste des équipements */}
-      <div>
-        {/* Utiliser la méthode map pour itérer sur chaque élément du tableau "equipments" */}
-        {equipments.map((equipment, index) => (
-          // Afficher chaque équipement dans un élément <p> avec une clé unique basée sur l'index
-          <p key={index}>{equipment}</p>
-        ))}
-      </div>
-    </ExpandableSection>
+      <ExpandableSection title="Équipements">
+        <div>
+          <div>
+            {equipments.map((equipment, index) => (
+              <p key={index}>{equipment}</p>
+            ))}
+          </div>
+        </div>
+      </ExpandableSection>
     </div>
   );
 };
