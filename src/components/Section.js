e1d9a36c1df3f288053243fc638abf09f11da327
Fantomeh <115874284+Fantomeh@users.noreply.github.com>
2023-06-25 20:47:26 +0200

    p

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,12 +2,13 @@
 import React, { useState, useEffect } from 'react';
 import { Article } from './Article';
 
+
 export const Section = () => {
   const [accommodations, setAccommodations] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('/logements.json') // Mettez à jour le chemin d'accès ici
+    fetch(process.env.PUBLIC_URL + '/logements.json') // Mettez à jour le chemin d'accès ici
       .then((response) => response.json())
       .then((data) => {
         setAccommodations(data);
